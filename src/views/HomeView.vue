28570ad2a8d621d804cde176b043cca80450251a
Snoozyman <barraculada@gmail.com>
2024-03-26 18:04:59 +0000

    Something to display

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,38 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <section class="hero is-medium">
+    <div class="hero-body">
+      <h1 class="title">Section</h1>
+      <h2 class="subtitle">
+        A simple container to divide your page into sections, like the one you're currently reading.
+      </h2>
+    </div>
+  </section>
+  <div class="columns">
+    <div class="column">
+      <section class="section">
+        <h1 class="title">Introduction</h1>
+        <p>Hello</p>
+      </section>
+    </div>
+    <!--
+    <div class="column">
+      <section class="section">
+        <h1 class="title">Introduction</h1>
+        <p>Hello</p>
+      </section>
+    </div>
+    -->
+  </div>
 </template>
+<style scoped>
+.hero-body {
+  background-image: url('../assets/background.jpg');
+  background-size: cover;
+}
+.title,
+.subtitle {
+  color: chocolate;
+}
+</style>
